412c9223d4b1ab83b822bc539cda29cd5642723a
So00s <105886252+So00s@users.noreply.github.com>
2023-03-26 21:37:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-'''
+"""
 функция которая считывает строки из файла
-'''
+"""
+
+
 def read_file(name):
-    with open(f"{name}", 'r', encoding = "UTF-8") as f:
+    with open(f"{name}", 'r', encoding="UTF-8") as f:
         list_line = f.readlines()
     return list_line
 
-#Разбиение на слова
+
+"""
+Разбиение на слова
+"""
+
 def read_word(list_lines):
     word = ""
     list_of_word = []
@@ -22,10 +28,12 @@ def read_word(list_lines):
         list_of_word.append(word)
         word = ""
     return list_of_word
-'''
-Перевод в цифры
-'''
-#перевод в цифры
+
+
+"""
+перевод в цифры
+"""
+
 def convert_to_numbers(string):
     list_of_num = []
     lfi5 = {41: "ё", 2: "1", 3: "2", 15: "tab", 16: "й", 30: "ф", 58: "Caps", 42: "L", 44: "я"}
@@ -91,16 +99,24 @@ def test_dict(num):
                 print(i, key)
     return 0
 
-#счетчик ходов
+
+"""
+счетчик ходов
+"""
+
 def count_of_move(list_of_numbers):
-    dict_of_row = {"tr": {"lfi5": {0: 41, 1: 2, 2: 3}, "lfi4": {3: 4}, "lfi3": {4: 5}, "lfi2": {5: 6, 6: 7}, "rfi2": {7\
-        : 8, 8: 9}, "rfi3": {9: 10}, "rfi4": {10: 11}, "rfi5": {11: 12, 12: 13, 13: 14}}, "mr": {"lfi5": {0: 15, 1: 16}\
-        , "lfi4": {2: 17}, "lfi3": {3: 18}, "lfi2": {4: 19, 5: 20, 6: 21}, "rfi2": {7: 22, 8: 23}, "rfi3": {9: 24}, "rf\
-        i4": {10: 25}, "rfi5": {11: 26, 12: 27, 13: 43}}, "hr": {"lfi5": {0: 58, 1: 30, 2: 31}, "lfi4": {3: 32}, "lfi3"\
-        : {4: 33}, "lfi2": {5: 34, 6: 35}, "rfi2": {7: 36, 8: 37},"rfi3": {9: 38}, "rfi4": {10: 39}, "rfi5": {11: 40,  \
-        12: 28, 13: ""}}, "dr": {"lfi5": {0: 42, 1: 44, 2: 45}, "lfi4": {3: 46}, "lfi3": {4: 47}, "lfi2": {5: 48, 6: 49\
-        }, "rfi2": {7: 50, 8: 51}, "rfi3": {9: 52}, "rfi4": {10: 53}, "rfi5": {11: 54, 12: "", 13: ""}}}
+    counter = 0
+    dict_of_row = {"tr": {"lfi5": {0: 41, 1: 2, 2: 3}, "lfi4": {3: 4}, "lfi3": {4: 5}, "lfi2": {5: 6, 6: 7}, "rfi\
+        2": {7: 8, 8: 9}, "rfi3": {9: 10}, "rfi4": {10: 11}, "rfi5": {11: 12, 12: 13, 13: 14}}, "mr": {"lfi\
+        5": {0: 15, 1: 16}, "lfi4": {2: 17}, "lfi3": {3: 18}, "lfi2": {4: 19, 5: 20, 6: 21}, "rfi2": {7: 22, 8: 23}, "r\
+        fi3": {9: 24}, "rfi4": {10: 25}, "rfi5": {11: 26, 12: 27, 13: 43}}, "hr": {"lfi5": {0: 58, 1: 30, 2: 31}, "lfi\
+        4": {3: 32}, "lfi3": {4: 33}, "lfi2": {5: 34, 6: 35}, "rfi2": {7: 36, 8: 37}, "rfi3": {9: 38}, "rfi\
+        4": {10: 39}, "rfi5": {11: 40, 12: 28, 13: ""}}, "dr": {"lfi5": {0: 42, 1: 44, 2: 45}, "lfi4": {3: 46}, "lfi\
+        3": {4: 47}, "lfi2": {5: 48, 6: 49}, "rfi2": {7: 50, 8: 51}, "rfi3": {9: 52}, "rfi4": {10: 53}, "rfi\
+        5": {11: 54, 12: "", 13: ""}}}
     count_moves = {"lfi5": 0, "lfi4": 0, "lfi3": 0, "lfi2": 0, "rfi2": 0, "rfi3": 0, "rfi4": 0, "rfi5": 0}
+    prev_position = {"lfi5": {"row": 0, "col": 0}, "lfi4": {"row": 0}, "lfi3": {"row": 0}, "lfi2": {"row": 0, "co\
+        l": 0}, "rfi2": {"row": 0, "col": 0}, "rfi3": {"row": 0}, "rfi4": {"row": 0}, "rfi5": {"row": 0, "col": 0}}
     for _ in list_of_numbers:
         for k, v in dict_of_row.items():
             for k1, v1 in v.items():
@@ -108,6 +124,9 @@ def count_of_move(list_of_numbers):
                     if _ == v2:
                         out = [k, k1, k2]
                         print(out)
+                        for k3, v3 in count_moves.items():
+                            if k2 == k3:
+                                v3 += counter
     lfi5_prev = 0
     pos_now = 0
     pos_prev = 0
