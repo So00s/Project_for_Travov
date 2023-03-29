5c82803b24190ce561ef8b97a76b9baa58afa428
kirillzinkin <61280624+kirillzinkin@users.noreply.github.com>
2023-03-29 15:22:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def main():
         for key, value in count_of_move(list_of_num).items():
             counter[key] += value
     print(counter)
+    counter = {"lfi5": 0, "lfi4": 0, "lfi3": 0, "lfi2": 0, "rfi2": 0, "rfi3": 0, "rfi4": 0, "rfi5": 0}
+    for _ in list_of_word:
+        list_of_num = (convert_vyzov_to_numbers(_))
+        print(list_of_num)
+        print(count_of_move(list_of_num))
+        for key, value in count_of_move(list_of_num).items():
+            counter[key] += value
+    print(counter)
 
 
 if __name__ == '__main__':
