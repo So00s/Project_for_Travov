6c5cf1f0aac850ec58c444d46e813c2fc3bbdd15
alexv <alex.volkovv@yandex.ru>
2023-04-05 23:47:22 +0300

    Last version

--- tests/test_func.py
+++ tests/test_func.py
@@ -13,6 +13,14 @@ def test_convert_to_numbers(a, expected_result):
     assert convert_to_numbers(temp) == expected_result
 
 
+@pytest.mark.parametrize("a, expected_result", [("ф", [52]),
+                                                ("а", [33]),
+                                                ("паф", [53, 33, 52])])
+def test_convert_vyzov_to_numbers(a, expected_result):
+    temp = a
+    assert convert_vyzov_to_numbers(temp) == expected_result
+
+
 @pytest.mark.parametrize("a, expected_result", [([42], {'lfi5': 2, 'lfi4': 0, 'lfi3': 0, 'lfi2': 0, 'rfi2': 0, 'rfi\
 3': 0, 'rfi4': 0, 'rfi5': 0}),
                                                 ([42, 35, 33, 46, 19, 54, 48, 21, 18, 49, 31], {'lfi5': 2, 'lfi4': 0, '\
